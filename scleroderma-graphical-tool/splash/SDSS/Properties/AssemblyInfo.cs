9195f8c1a2cc761db6adc859149b0c4f9ed7eba6
Karl-Marka <karl.marka@ttu.ee>
2016-07-01 14:03:21 -0400

    Update 2. July

---
+++ scleroderma-graphical-tool/splash/SDSS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SDSS")]
+[assembly: AssemblyDescription("Author: Karl Märka")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Celdara Medical")]
+[assembly: AssemblyProduct("SDSS GUI")]
+[assembly: AssemblyCopyright("Copyright © Celdara Medical 2016")]
+[assembly: AssemblyTrademark("Scleroderma Scoring Tool")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("530e37f4-05a5-420d-ab04-eda8ad96978e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
